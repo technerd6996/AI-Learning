49eafc14200a052dfbaca79ba2eede5283a556d4
technerd6996 <vedavidyadaran@outlook.com>
2026-04-08 00:04:09 +0530

    Added basic History Logic to the Code

--- first_ai.py
+++ first_ai.py
@@ -11,25 +11,33 @@ if not api_key:
 
 client=Groq(api_key=api_key)
 
+# Adding history List Dictionary to make it remember of the previous conversations
+
+history = []
+
+history.append({
+    "role":"system",
+    "content": "You are an expert SRE Engineer. Answer questions clearly and technically deep."
+})
+
 # Function to ask the AI Anything
 
+
 def ask_ai(question):
+    history.append({"role":"user", "content": question})
+
     response = client.chat.completions.create(
         model="meta-llama/llama-4-scout-17b-16e-instruct",
-        messages=[
-            {
-                "role":"system",
-                "content":"You are an expert SRE Engineer. Answer questions clearly and technically deep."
-            },
-            {
-                "role":"user",
-                "content":question
-            }
-        ]
+        messages=history
     )
     return response.choices[0].message.content
+    history.append({"role":"assistant", "content": answer})
+    return answer
 
 #Ask Actual Question 
 
-answer = ask_ai("List the possible reasons for High CPU utilization Linux Servers")
-print(answer)
+answer1 = ask_ai("What is SLI?")
+print(answer1)
+
+answer2 = ask_ai("Give me an example of what you just explained")
+print(answer2)
